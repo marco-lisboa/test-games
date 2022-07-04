7463c0190735788290bf15e0b9da863ee3675d9c
marco-lisboa <93157451+marco-lisboa@users.noreply.github.com>
2022-07-04 13:36:18 -0300

    header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContainerComponent } from './components/container/container.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    // canActivate: [AuthGaurd],
+    component: ContainerComponent,
+    //loadChildren: () => import('./modulos/modulos.module').then(m => m.ModulosModule)
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContainerComponent } from './components/container/container.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SwiperModule } from "swiper/angular";
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    ContainerComponent,
+    HeaderComponent
+   ],
   imports: [
     BrowserModule,
+    SwiperModule,
     AppRoutingModule
   ],
   providers: [],
